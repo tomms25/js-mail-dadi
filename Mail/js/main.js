bc3a4e2df2817d6bbde81378aa3161088f5ef6fb
tomms25 <tommasobaccano@gmail.com>
2022-11-13 12:07:26 +0100

    Completamento dadi e mail

---
+++ Mail/js/main.js
@@ -0,0 +1,35 @@
+
+// Chiedi all’utente la sua email,
+// controlla che sia nella lista di chi può accedere,
+// stampa un messaggio appropriato sull’esito del controllo.
+
+
+
+
+
+// Chiedere all'utente email
+let user_mail = prompt("Inserisci qui la tua email!");
+console.log(user_mail);
+
+// Controllare che sia nella lista di chi può accedere
+
+let listaMail = ["beatrice.baccano@yahoo.it","michela.franchini@hotmail.it","stefano.zina@gmail.it","gianluca.pesce@yahoo.it","adriano.grimaldi@yahoo.it","andrea.castenetto@gmail.it"];
+console.log(listaMail);
+
+let mailIsValid = false;  
+
+for (let l = 0; l <= listaMail.length; l++ ) {
+    if (user_mail == listaMail[l]) {
+        mailIsValid = true;
+    }
+}
+
+// Stampare un messaggio appropriato sull'esito del controllo
+
+if (mailIsValid == false) {
+    document.getElementById("mail").innerHTML = "Gentile utente la sua mail <i>" + user_mail + "</i>  risulta errata";
+} else {
+    document.getElementById("mail").innerHTML = "Gentile utente la sua mail " + user_mail + " risulta corretta" ;
+}
+
+console.log(mailIsValid);
